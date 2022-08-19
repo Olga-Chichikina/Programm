07f98456572bee1edaf5f7f5cf8bd3b3c79dcd9b
Olga-6 <olga.chichikin1968@mail.ru>
2022-08-19 23:45:06 +0300

    Сумма случайных чисел

---
+++ lector/proekt5/Program.cs
@@ -0,0 +1,8 @@
+﻿// сумма случайных чисел
+int numbera = new Random().Next(0,10); //случайные числа 1,2,3,4,5,6,7,8,9,10
+Console.WriteLine("Случайное число А="+numbera);
+int numberb = new Random().Next(0,10);
+Console.WriteLine("Случайное число Б="+numberb);
+Console.WriteLine("Сумма случайных чисел равна");
+Console.WriteLine(numbera+numberb);
+
