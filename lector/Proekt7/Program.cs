421005099af3a02ffe6d1a831fa16f22f5683750
Olga-6 <olga.chichikin1968@mail.ru>
2022-08-20 00:41:42 +0300

    Нахождение максимального числа

---
+++ lector/Proekt7/Program.cs
@@ -0,0 +1,12 @@
+﻿// нахождение максимального числа
+int a = 3;
+int b = 6;
+int c = 2;
+int d = 8;
+int e = 0;
+int max = a;
+if (b>max) max=b ; 
+if (c>max) max=c ;
+if (d>max) max=d ;
+if (e>max) max=e ;
+Console.Write("Максимальное число"+ max);
