add7bc67266201b0b15fa1a766121b513f11f628
Olga-6 <olga.chichikin1968@mail.ru>
2022-08-19 11:17:43 +0300

    proekt2-ввод имени

--- lector/proekt2/Program.cs
+++ lector/proekt2/Program.cs
@@ -3,6 +3,10 @@ string name = Console.ReadLine() ;
 Console.WriteLine("Привет, " + name + "! ");
 //Console.WriteLine(name);
 
+Console.Write("Введите данные");
+string name = Console.ReadLine() ;
+Console.Write("Привет, " + name + "! ");
+//ввывод в одну строку;
 
 
 
