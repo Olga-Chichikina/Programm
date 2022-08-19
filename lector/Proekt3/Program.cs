ba46ffb5ffe8181a75e4db87e9798cfa0526df03
Olga-6 <olga.chichikin1968@mail.ru>
2022-08-19 12:23:41 +0300

    lector

---
+++ lector/Proekt3/Program.cs
@@ -0,0 +1,14 @@
+﻿// программа сложения двух целых чисел 1-вариант
+int number1=1;
+int number2=3;
+Console.Write("Сумма равна");
+Console.Write(number1+number2);
+
+// программа сложения двух целых чисел 2-вариант
+int numbera = 10;
+int numberb = 3;
+int result= numbera + numberb ;
+Console.Write("Сумма равна");
+Console.Write(result);
+
+
