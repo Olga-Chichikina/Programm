30bfe5aaa84c70a1812a45c5219b2cf14c9e4eee
Olga-6 <olga.chichikin1968@mail.ru>
2022-08-20 00:16:32 +0300

    Особое приветствие

---
+++ lector/proekt6/Program.cs
@@ -0,0 +1,14 @@
+﻿// Особое приветствие пользователя
+Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это Маша!");
+}
+else
+{
+    Console.WriteLine("Привет!");
+    Console.Write(username) ;
+}
+
