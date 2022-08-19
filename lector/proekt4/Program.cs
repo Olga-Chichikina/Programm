58dff586fe3d8dd8acad03402682056373a52753
Olga-6 <olga.chichikin1968@mail.ru>
2022-08-19 16:19:19 +0300

    Нахождение частного целых,вещественных чисел

---
+++ lector/proekt4/Program.cs
@@ -0,0 +1,17 @@
+﻿// найти частное двух целых чисел
+/*
+int numbera = 10;
+int numberb = 5;
+Console.WriteLine("Частное равно");
+Console.WriteLine(numbera/numberb);
+*/
+
+// найти частное двух вещественных  чисел
+double numbera=13;
+double numberb=5;
+Console.WriteLine("Частное равно");
+Console.WriteLine( numbera / numberb );
+
+
+
+
