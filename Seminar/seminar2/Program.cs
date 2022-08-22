3ed09a9fb879fd5e9647c3966cfd05130eb89f20
Olga-6 <olga.chichikin1968@mail.ru>
2022-08-22 16:00:15 +0300

    1 задача

---
+++ Seminar/seminar2/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+int FindBiggerDigit( int number)
+{
+int ed = number % 10;
+int dec= number / 10;
+int max;
+if ( ed >dec)  max=ed;
+else max = dec;
+return max;
+}
+
+int randNumber= new Random().Next(10,100);
+int biggerdigit= FindBiggerDigit(randNumber);
+Console.WriteLine( randNumber);
+Console.WriteLine( biggerdigit);
+
+
+
