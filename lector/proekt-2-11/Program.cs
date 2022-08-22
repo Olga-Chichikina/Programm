75493acd729ddc8811b57cd02a62a59e02085035
Olga-6 <olga.chichikin1968@mail.ru>
2022-08-22 12:26:42 +0300

    метод ввода массива, метод печати массива из генератора случайных чисел

---
+++ lector/proekt-2-11/Program.cs
@@ -0,0 +1,25 @@
+﻿void FillMassiv(int[]collection)
+{
+  int length = collection.Length;
+  int index=0;
+  while (index<length)
+  {
+  collection[index]=new Random().Next(1,10);
+  index++;
+  }
+}
+
+void PrintMassiv(int[] col)
+{
+int count = col.Length;
+int position = 0;
+while ( position < count )
+{
+    Console.WriteLine(col[position]);
+    position++;
+} 
+}
+
+int[] massiv= new int[10];
+FillMassiv(massiv);
+PrintMassiv(massiv);
