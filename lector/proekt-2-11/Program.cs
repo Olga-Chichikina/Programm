8a1c1f02c8d0460f9fd4dbb52dd9be4565b73656
Olga-6 <olga.chichikin1968@mail.ru>
2022-09-01 10:50:44 +0300

    массивы ввод, печать, индексы

--- lector/proekt-2-11/Program.cs
+++ lector/proekt-2-11/Program.cs
@@ -23,3 +23,28 @@ while ( position < count )
 int[] massiv= new int[10];
 FillMassiv(massiv);
 PrintMassiv(massiv);
+
+ int Index(int[] massiv , int find )
+ {
+  int position=0;
+  int i =0;
+    while( i< massiv.Length)
+    {
+      if( massiv[i] == find)
+      { 
+        position = i ;
+        break; 
+      }
+      else
+      {
+        position= -1;
+      }
+      i++;
+    }
+  return position;
+ }
+int pos = Index ( massiv, 21);
+Console.WriteLine();
+Console.WriteLine(pos) ;
+
+
